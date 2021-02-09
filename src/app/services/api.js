1b289fbccd8fe1acbac61390e862f49e9d378660
Emanoel Johannes <emanoeljohannes@hotmail.com>
2021-02-09 14:01:34 -0300

    Update api.js

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({
-  // baseURL: 'https://cacbempreenderapp.org.br/backend', 
-  baseURL: 'http://192.168.15.8:3333', 
+const api = axios.create({  
+  baseURL: 'https://api.utol.com.br/', 
+  // baseURL: 'http://192.168.15.8:3333', 
 });
 
 api.defaults.timeout = 30000;
