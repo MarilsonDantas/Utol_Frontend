f2adf665982ae6a1f7f53c50541b43d7abbbb8c8
Emanoel Johannes <emanoeljohannes@hotmail.com>
2021-07-08 12:07:48 -0300

    Atualizações Build 2

---
+++ build/precache-manifest.fab4c74e22af5f5523969af1c5486f8f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb6fe0dbf81e7a670ede123be1b3ea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e084eae0b8455b7369b4",
+    "url": "/static/css/3.60920e77.chunk.css"
+  },
+  {
+    "revision": "a57e1858eb3b91c40b73",
+    "url": "/static/css/main.b196356c.chunk.css"
+  },
+  {
+    "revision": "784aca82f99d75063826",
+    "url": "/static/js/0.567fbe70.chunk.js"
+  },
+  {
+    "revision": "ecf05d27d85db63fd43c",
+    "url": "/static/js/10.079c088d.chunk.js"
+  },
+  {
+    "revision": "f092494a2c5bf252b0fe",
+    "url": "/static/js/11.38c6ccdc.chunk.js"
+  },
+  {
+    "revision": "7fb8fe20a1a19ef89ba2",
+    "url": "/static/js/12.a4c52eff.chunk.js"
+  },
+  {
+    "revision": "564eabe810ca8853271a",
+    "url": "/static/js/13.731a2474.chunk.js"
+  },
+  {
+    "revision": "5ec3daec0aa7cbc31af6",
+    "url": "/static/js/14.cc9bc141.chunk.js"
+  },
+  {
+    "revision": "ec51a1cc134aaba7d5d9",
+    "url": "/static/js/15.20f837a9.chunk.js"
+  },
+  {
+    "revision": "8ac637e58af4afe8e87b",
+    "url": "/static/js/16.77b1cb4b.chunk.js"
+  },
+  {
+    "revision": "937156f8b969b206879d",
+    "url": "/static/js/17.84e9507d.chunk.js"
+  },
+  {
+    "revision": "552986869206e2445ef0",
+    "url": "/static/js/18.4d421775.chunk.js"
+  },
+  {
+    "revision": "2d39b3fefa9c1e8332a9",
+    "url": "/static/js/19.7b2da6c8.chunk.js"
+  },
+  {
+    "revision": "21aeaa02ce1eccfb53e9",
+    "url": "/static/js/20.8f8ef6ba.chunk.js"
+  },
+  {
+    "revision": "171a011df935ea58a1ad",
+    "url": "/static/js/21.34c46520.chunk.js"
+  },
+  {
+    "revision": "f6cac65f737c2958bc76",
+    "url": "/static/js/22.e09156e9.chunk.js"
+  },
+  {
+    "revision": "e084eae0b8455b7369b4",
+    "url": "/static/js/3.d0f67c3b.chunk.js"
+  },
+  {
+    "revision": "32df4fa93a9ee72b17be",
+    "url": "/static/js/4.dde4a7bb.chunk.js"
+  },
+  {
+    "revision": "30ce492387d13dc2ed17",
+    "url": "/static/js/5.12618685.chunk.js"
+  },
+  {
+    "revision": "89caa043a0048f2cd615",
+    "url": "/static/js/6.79500877.chunk.js"
+  },
+  {
+    "revision": "5cadcf6ea6914f2fa9f3",
+    "url": "/static/js/7.a9e0de72.chunk.js"
+  },
+  {
+    "revision": "bd528919a68da13f855d",
+    "url": "/static/js/8.43033220.chunk.js"
+  },
+  {
+    "revision": "00d83cbf75e4d23f8d42",
+    "url": "/static/js/9.d2b5a460.chunk.js"
+  },
+  {
+    "revision": "a57e1858eb3b91c40b73",
+    "url": "/static/js/main.8f0957de.chunk.js"
+  },
+  {
+    "revision": "749257ba583b244e6d2f",
+    "url": "/static/js/runtime~main.74f345e7.js"
+  },
+  {
+    "revision": "612532ffbb2b443a080d9deba6b4b167",
+    "url": "/static/media/balanco.612532ff.png"
+  },
+  {
+    "revision": "bc153341d3199549f897deb278a396dd",
+    "url": "/static/media/balanco_ativo.bc153341.png"
+  }
+]);
